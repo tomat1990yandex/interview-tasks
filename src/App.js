28e7275a68cfac0b5fb5493becbf87ac31e52bde
nexusagentdo1 <emino71683@rambler.ru>
2023-09-24 14:36:10 +0500

    task 1+2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='dialog-container'>
+      <div className='dialog'>
+        <div className='dialog__header'>
+          <div className='dialog__header-title-container'>
+            <div className='dialog__header-title'>
+              Ссъешь ещё этих мягких французских булок, да выпей чаю
+            </div>
+            <div className='dialog__header-subtitle'>
+              Съешь ещё этих мягких французских булок, да выпей чаю
+            </div>
+          </div>
+          <button className='dialog__header-button'>
+            Кнопка закрытия
+          </button>
+        </div>
+        <div className='dialog__content'>
+          <div className='dialog__content-block dialog__content-block_left'>
+            Left
+          </div>
+          <div className='dialog__content-block dialog__content-block_right'>
+            <div className='content'>
+              <span>Top</span>
+              <span>Center</span>
+              <span>Bottom</span>
+            </div>
+          </div>
+        </div>
+        <div className='dialog__footer'>
+          Ссъешь ещё этих мягких французских булок, да выпей чаю<br />
+          ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<br />
+          Ссъешь ещё этих мягких французских булок, да выпей чаю
+        </div>
+      </div>
     </div>
   );
 }
